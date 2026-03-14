06476bf28475bf9700dd18740ca53cd723c2602d
Rinie <buyukkeklikrana@gmail.com>
2026-03-14 17:19:41 +0300

    Sis version 1: cases 1,3,4 were added.

--- src/main/java/com/sis/StudentInformationSystemV1.java
+++ src/main/java/com/sis/StudentInformationSystemV1.java
@@ -10,7 +10,7 @@ public class StudentInformationSystemV1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int choice=0;
+        int choice = 0;
         List<Student> studentList = new ArrayList<>();
 
         do {
@@ -30,38 +30,68 @@ public class StudentInformationSystemV1 {
                     System.out.println("Please enter the age");
                     int age = scanner.nextInt();
                     scanner.nextLine();
-                    Student student = new Student(age,name,id);
+                    Student student = new Student(age, name, id);
                     studentList.add(student);
                     System.out.println("Added");
                     break;
                 case 2:
-                    System.out.println("You can now update a student");
+                    String newName=null;
+                    int newAge=-1;
+                    System.out.println("Which student you want to update, write the id.");
+                    id = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("To update name press 1, to update age press 2");
+                    int updateChoice = scanner.nextInt();
+                    scanner.nextLine();
+                    if(updateChoice==1){
+                        System.out.println("Please enter the new name.");
+                         newName = scanner.nextLine();
+                    }else if(updateChoice==2){
+                        System.out.println("Please enter the new age");
+                         newAge = scanner.nextInt();
+                        scanner.nextLine();
+                }
+
+
+                    for (int i = 0; i < studentList.size(); i++) {
+                        if (studentList.get(i).getId() == id) {
+                          Student updateThisStudent =  studentList.get(i);
+                          if(updateChoice==2){
+                          updateThisStudent.setAge(newAge);
+                        }else if(updateChoice==1){
+                          updateThisStudent.setName(newName);
+                          }
+                        }
+                    }
+
                     break;
                 case 3:
                     System.out.println("Choose the id you want to delete");
                     id = scanner.nextInt();
                     scanner.nextLine();
-                    int indexToDelete=-1;
+                    int indexToDelete = -1;
 
                     Student deletedStudent = null;
-                    for(int i =0; i<studentList.size();i++){
-                       if(studentList.get(i).getId() == id ) {
+                    for (int i = 0; i < studentList.size(); i++) {
+                        if (studentList.get(i).getId() == id) {
                             indexToDelete = i;
 
                             deletedStudent = studentList.get(i);
-                       }
+                        }
                     }
-                    if(indexToDelete!=-1)
-                    {studentList.remove(indexToDelete);
+                    if (indexToDelete != -1) {
+                        studentList.remove(indexToDelete);
                         System.out.println("Deleted: " + deletedStudent.toString());
-                    }else{
+                    } else {
                         System.out.println("No id match");
                     }
                     break;
                 case 4:
-                    if(studentList.size()!=0){for(Student s: studentList){
-                        System.out.println(s.toString());
-                    }}else{
+                    if (studentList.size() != 0) {
+                        for (Student s : studentList) {
+                            System.out.println(s.toString());
+                        }
+                    } else {
                         System.out.println("No students were found.");
                     }
                     break;
