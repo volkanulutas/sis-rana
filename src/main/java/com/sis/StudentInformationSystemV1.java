44fe89902df0ef1bef60f490ad21c928d6a2321a
Rinie <buyukkeklikrana@gmail.com>
2026-03-15 13:16:54 +0300

    Sis version 1: cases 1,3,4 were added.

--- src/main/java/com/sis/StudentInformationSystemV1.java
+++ src/main/java/com/sis/StudentInformationSystemV1.java
@@ -23,7 +23,7 @@ public class StudentInformationSystemV1 {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println( Colors.RED +" -----student adding system---- \n"+ Colors.RESET+ "Please enter the ID.");
+                    System.out.println(Colors.RED + " -----student adding system---- \n" + Colors.RESET + "Please enter the ID.");
                     int id = scanner.nextInt();
                     scanner.nextLine();
                     System.out.println("Please enter the name");
@@ -33,56 +33,57 @@ public class StudentInformationSystemV1 {
                     scanner.nextLine();
                     Student student = new Student(age, name, id);
                     studentList.add(student);
-                    System.out.println(Colors.RED + "------" +name +" was added---------" + Colors.RESET);
+                    System.out.println(Colors.RED + "------" + name + " was added---------" + Colors.RESET);
                     break;
                 case 2:
 
-                    if(studentList.size()>0 ) {
-                        int currentIndex=0;
-                        String newName = "";
+                    if (studentList.size() > 0) {
+                        int currentIndex = -1;
+                        String newName = "X";
+                        Student updateThisStudent = null;
                         int newAge = 0;
                         System.out.println(Colors.RED + "-----student update system----- \n" + Colors.RESET + "Write the ID you want to update");
                         id = scanner.nextInt();
                         scanner.nextLine();
+                        int updateChoice = 0;
+                        for (int i = 0; i < studentList.size(); i++) {
+                            currentIndex = i;
+                            if (studentList.get(i) != null && (studentList.get(i).getId() == id)) {
+                                System.out.println("To update the name press 1, to update the age press 2");
+                                updateChoice = scanner.nextInt();
+                                scanner.nextLine();
+                                updateThisStudent = studentList.get(i);
+                                if (updateChoice == 1) {
+                                    System.out.println("Please enter the new name.");
+                                    newName = scanner.nextLine();
 
-                        System.out.println("To update the name press 1, to update the age press 2");
-                        int updateChoice = scanner.nextInt();
-                        scanner.nextLine();
-                        if (updateChoice == 1) {
-                            System.out.println("Please enter the new name.");
-                            newName = scanner.nextLine();
-                            if(newName.equals(studentList.get(currentIndex))){
-                                System.out.println(Colors.RED+"The name cannot be the same."+Colors.RESET);
-                            }
-                        } else if (updateChoice == 2) {
-                            System.out.println("Please enter the new age");
-                            newAge = scanner.nextInt();
-                            scanner.nextLine();
-                        }else{
-                            System.out.println(Colors.RED + "Please enter a valid number."+  Colors.RESET);
-                            //It should repeat itself from here
-                        }
-
-                        for ( int i = 0; i < studentList.size(); i++) {
-                            currentIndex=i;
-                            if (studentList.get(i).getId() == id) {
-                                Student updateThisStudent = studentList.get(i);
+                                } else if (updateChoice == 2) {
+                                    System.out.println("Please enter the new age");
+                                    newAge = scanner.nextInt();
+                                    scanner.nextLine();
+                                } else {
+                                    System.out.println(Colors.RED + "Please enter a valid number." + Colors.RESET);
+                                    //It should repeat itself from here
+                                }
                                 if (updateChoice == 2) {
                                     updateThisStudent.setAge(newAge);
                                 } else if (updateChoice == 1) {
                                     updateThisStudent.setName(newName);
                                 }
+                            } else if (currentIndex < 0) {
+                                System.out.println(Colors.RED + "No student match found to update." + Colors.RESET);
                             }
                         }
 
-                    }else{
-                        System.out.println(Colors.RED+"No students were found to update."+ Colors.RESET);
+
+                    }else {
+                        System.out.println(Colors.RED + "No students were found to update." + Colors.RESET);
                     }
                     break;
 
 
-
                 case 3:
+                    if(studentList.isEmpty()==false){
                     System.out.println("Choose the id you want to delete");
                     id = scanner.nextInt();
                     scanner.nextLine();
@@ -98,27 +99,29 @@ public class StudentInformationSystemV1 {
                     }
                     if (indexToDelete != -1) {
                         studentList.remove(indexToDelete);
-                        System.out.println("Deleted: " + deletedStudent.toString());
+                        System.out.println("Deleted: \n  " + deletedStudent.toString());
                     } else {
                         System.out.println("No id match");
+                    }}else {
+                        System.out.println(Colors.RED + "No students were found to delete." + Colors.RESET);
                     }
                     break;
                 case 4:
                     if (studentList.size() != 0) {
                         System.out.println("ID      NAME        AGE\n" +
-                                "-----------------------" );
+                                "-----------------------");
                         for (Student s : studentList) {
                             System.out.println(s.showOnlyDetails());
                         }
                     } else {
-                        System.out.println("No students were found.");
+                        System.out.println(Colors.RED +"No students were found." + Colors.RESET);
                     }
                     break;
                 case 5:
                     System.out.println("You can exit");
                     break;
                 default:
-                    System.out.println(Colors.RED + "Please press a valid number."+ Colors.RESET);
+                    System.out.println(Colors.RED + "Please press a valid number." + Colors.RESET);
             }
         } while (choice != 5);
 
